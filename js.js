b6c5833a13c66e6f2f15b2a38e2f9d12f7f0da5b
Pekarski <pekarski#gmail.com>
2021-12-13 02:20:53 +0300

    HW-1

---
+++ js.js
@@ -0,0 +1,87 @@
+let a1 = 5 % 3;
+console.log(a1, typeof a1);
+let a2 = 3 % 5;
+console.log(a2, typeof a2);
+let a3 = 5 + '3';
+console.log(a3, typeof a3);
+let a4 = '5' - 3;
+console.log(a4, typeof a4);
+let a5 = 75 + 'кг';
+console.log(a5, typeof a5);
+let a6 = 785 * 653;
+console.log(a6, typeof a6);
+let a7 = 100 / 25;
+console.log(a7, typeof a7);
+let a8 = 0 * 0;
+console.log(a8, typeof a8);
+let a9 = 0 / 2;
+console.log(a9, typeof a9);
+let a10 = 89 / 0;
+console.log(a10, typeof a10);
+let a11 = 98 + 2;
+console.log(a11, typeof a11);
+let a12 = 5 - 98;
+console.log(a12, typeof a12);
+let a13 = (8 + 56 * 4) / 5;
+console.log(a13, typeof a13);
+let a14 = (9 - 12) * 7 / (5 + 2);
+console.log(a14, typeof a14);
+let a15 = +"123";
+console.log(a15, typeof a15);
+let a16 = 1 || 0;
+console.log(a16, typeof a16);
+let a17 = false || true;
+console.log(a17, typeof a17);
+let a18 = true > 0;
+console.log(a18, typeof a18);
+
+
+
+
+let width = 10,
+    height = 23;
+console.log(SPryam = width * height)
+
+
+
+let p = 3.14,
+    h = 10,
+    VCilindra = (p*a7**2/4*h);
+    console.log(VCilindra)
+
+
+let R = 5,
+SKruga = p*R**2;
+console.log(SKruga);
+
+let a = 5,
+    b = 7,
+    STrap = 1/2*h*(a+b);
+    console.log(STrap)
+
+
+
+// 7 задание
+    sk = 2000000
+    pk = 10
+
+   plat = sk*(10/(1+10)-60-1)
+   console.log(plat)
+
+
+//    let s = 2000000,
+//        pk = 0.1,
+//        yk = 5,
+//        pe =(s*per)*year;
+//        console.log(pe);
+
+
+// задание 8
+
+// let au = 8,
+//     bu = 3,
+//     16 = 8+2*(x-3)
+
+
+
+//     console.log(x)
